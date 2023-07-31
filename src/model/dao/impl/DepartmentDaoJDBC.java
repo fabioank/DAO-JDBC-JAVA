5cc69d7d1eebf91ba8bb08a2d170076a48f0a67f
Fabio Kulka <fabiokulka.67@gmail.com>
2023-07-31 11:37:10 -0300

    Implemented DepartmentInsert

---
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,57 @@
+package model.dao.impl;
+
+import model.dao.DepartmentDao;
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+
+    private Connection conn;
+
+    public DepartmentDaoJDBC(Connection conn){
+        this.conn = conn;
+    }
+
+    @Override
+    public void insert(Department obj) {
+        PreparedStatement st = null;
+        try{
+            st = conn.prepareStatement(
+                    "INSERT INTO department " +
+                            "(Name) " +
+                            "VALUES " +
+                            "(?)");
+            st.setString(1, obj.getName());
+
+            int rows = st.executeUpdate();
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+    @Override
+    public void update(Department obj) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Department findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Department> findAll() {
+        return null;
+    }
+}

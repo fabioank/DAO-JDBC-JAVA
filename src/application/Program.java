a6f2b38fd38eb5aca709b7b9c12770a68719fdf3
Fabio Kulka <fabiokulka.67@gmail.com>
2023-07-31 10:29:05 -0300

    Implementado insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 
@@ -29,6 +30,11 @@ public class Program {
         for(Seller obj: sellerFindAll){
             System.out.println(obj);
         }
+
+        System.out.println("\n===TEST 4: seller insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! " + newSeller.getId());
 
 
 
